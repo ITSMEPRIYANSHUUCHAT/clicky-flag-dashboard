4d0da2ec4d4d68b2797c15e133f7a5796f5f2e59
Testers <techggdevelopers@gmail.com>
2025-03-28 16:14:49 +0000

    misc

--- src/lib/flagStore.ts
+++ src/lib/flagStore.ts
@@ -3,7 +3,7 @@ import { create } from 'zustand';
 import { toast } from 'sonner';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export interface Flag {
   _id: string;
